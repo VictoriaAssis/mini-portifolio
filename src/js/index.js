3034ac5fca9718fdbcdd74b4ae0e36e9d69f0c5f
Victoria <139024491+VictoriaAssis@users.noreply.github.com>
2023-08-08 16:25:42 -0400

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+
+/// passo 1 - dar um jeito de pegar os elementos que representam as abas no HTML
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach((aba) => {
+  // passo 2 - dar um jeito de identificar o clique no elemento da aba
+  aba.addEventListener("click", function () {
+    if (aba.classList.contains("selecionado")) return;
+
+    selecionarAba(aba);
+    mostrarInformacoesDaAba(aba);
+  });
+});
+
+function selecionarAba(aba) {
+  // passo 3 - desmarcar a aba selecionada
+  const abaSelecionada = document.querySelector(".aba.selecionado");
+  abaSelecionada.classList.remove("selecionado");
+
+  // passo 4 - marcar a aba clicada como selecionado
+  aba.classList.add("selecionado");
+}
+
+function mostrarInformacoesDaAba(aba) {
+  const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`;
+  const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba);
+
+  // passo 5 - esconder o conteúdo anterior
+  const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+  informacaoSelecionada.classList.remove("selecionado");
+
+  // passo 6 - mostrar o conteúdo da aba selecionada
+  informacaoASerMostrada.classList.add("selecionado");
+}
